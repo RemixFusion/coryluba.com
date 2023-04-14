dd12361caa30bc3407356981cc6a17bba5e02883
Remix <32577626+RemixFusion@users.noreply.github.com>
2023-04-14 18:29:14 -0400

    Update index.html / Update ProjectDetailsModal.jsx

--- src/components/ProjectDetailsModal.jsx
+++ src/components/ProjectDetailsModal.jsx
@@ -91,7 +91,7 @@ const ProjectDetailsModal = ({ darkTheme, projectDetails }) => {
                                 preload="auto"
                                 width="100%"
                                 poster="MY_VIDEO_POSTER.jpg"
-                                data-setup="{}"
+                                data-setup='{}'
                             >
                             <source src="{video}" type="video/mp4" />
                             <p class="vjs-no-js">
