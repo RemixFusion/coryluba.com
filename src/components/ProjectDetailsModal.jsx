d3c5fe9e35e803b97ca8affab7a30fd5dfd004de
Remix <32577626+RemixFusion@users.noreply.github.com>
2023-04-14 20:13:06 -0400

    Video.js upgrade

--- src/components/ProjectDetailsModal.jsx
+++ src/components/ProjectDetailsModal.jsx
@@ -98,7 +98,9 @@ const ProjectDetailsModal = ({ darkTheme, projectDetails }) => {
                             To view this video please enable JavaScript, and consider upgrading to a web browser that <a href="https://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a>
                             </p>
                             </video>
-                            <script src="https://vjs.zencdn.net/8.0.4/video.min.js"></script>
+                              <script>
+                                const a = videojs('portfolio-video');
+                              </script>
                             </div>
                         ))}
                     </Slider>
