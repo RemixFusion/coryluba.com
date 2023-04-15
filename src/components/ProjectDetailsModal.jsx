a4a96ee74fb12c66f44ac8cc5fab85c0ad8b4fbf
Remix <32577626+RemixFusion@users.noreply.github.com>
2023-04-14 21:02:54 -0400

    Update ProjectDetailsModal.jsx

--- src/components/ProjectDetailsModal.jsx
+++ src/components/ProjectDetailsModal.jsx
@@ -85,9 +85,7 @@ const ProjectDetailsModal = ({ darkTheme, projectDetails }) => {
                           projectDetails?.sliderVideos?.map((video, index) => (
                             <div className="item" key={index}>
                             <video id="stream1" class="test1 video-js vjs-default-skin" data-setup='{ "poster": "img/stream1-1080p.png"}'controls preload="auto">
-                            <source src={video} label="1080p" selected="true" type="application/x-mpegURL">
-                            </video>
-                            <source src={video} type="video/mp4" />
+                            <source src={video} label="1080p" selected="true" type="application/x-mpegURL" />
                             <p class="vjs-no-js">
                             To view this video please enable JavaScript, and consider upgrading to a web browser that <a href="https://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a>
                             </p>
